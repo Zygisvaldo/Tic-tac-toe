4b68f9e46690e36e6b2cf498797d5d7ac48633f1
Zygisvaldo <zygiodeze@gmail.com>
2023-11-01 15:28:52 +0100

    fix: minor restructuring of initial state and helper functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,60 +3,18 @@ import GameBoard from "./components/GameBoard/GameBoard";
 import Players from "./components/Players/Players";
 import Header from "./components/layout/Header";
 import Log from "./components/Log/Log";
-import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver/GameOver";
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-const deriveActivePlayers = (gameTurns) => {
-  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
-    return "O";
-  } else {
-    return "X";
-  }
-};
-
-const deriveWinner = (gameBoard, players) => {
-  let winner;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
-    const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].col];
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
-
-    if (
-      firstSquareSymbol &&
-      firstSquareSymbol === secondSquareSymbol &&
-      firstSquareSymbol === thirdSquareSymbol
-    ) {
-      winner = players[firstSquareSymbol];
-    }
-  }
-  return winner;
-};
-
-const deriveGameBoard = (gameTurns) => {
-  let gameBoard = [...initialGameBoard.map((array) => [...array])];
-
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
-  return gameBoard;
-};
+import { PLAYERS } from "./initial-game-state";
+import {
+  deriveActivePlayers,
+  deriveGameBoard,
+  deriveWinner,
+} from "./game-helper-functions";
 
 const App = () => {
-  const [players, setPlayers] = useState({
-    X: "Player 1",
-    O: "Player 2",
-  });
+  const [players, setPlayers] = useState(PLAYERS);
   const [gameTurns, setGameTurns] = useState([]);
+
   const activePlayer = deriveActivePlayers(gameTurns);
   const gameBoard = deriveGameBoard(gameTurns);
   const winner = deriveWinner(gameBoard, players);
---
+++ src/game-helper-functions.js
@@ -0,0 +1,41 @@
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import { INITIAL_GAME_BOARD } from "./initial-game-state";
+
+export const deriveActivePlayers = (gameTurns) => {
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    return "O";
+  } else {
+    return "X";
+  }
+};
+
+export const deriveWinner = (gameBoard, players) => {
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].col];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+    }
+  }
+  return winner;
+};
+
+export const deriveGameBoard = (gameTurns) => {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+  return gameBoard;
+};
